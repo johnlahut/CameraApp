09f0ea4dbbd8ca8c876b39e5ac3d8fdeae764e8b
lahut13 <lahut13@gmail.com>
2016-09-22 01:42:48 -0400

    First app created, simple camera app that opens and stores pictures taken

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var TakePhoto: UIButton!
+    @IBOutlet weak var PhotoLibrary: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func TakePhotoAction(sender: UIButton) {
+        
+        let imgPicker = UIImagePickerController()
+        
+        imgPicker.delegate = self;
+        imgPicker.sourceType = .Camera
+        imgPicker.showsCameraControls = true
+        imgPicker.editing = true
+        
+        let graphRect:CGRect = CGRectMake(0, 0, 300, 300)
+        let overlayView:UIView = UIView(frame: graphRect)
+        
+        overlayView.backgroundColor = UIColor(colorLiteralRed: 50.0/255.0, green: 50.0/255.0, blue: 50.0/225.0, alpha: 1.0)
+        
+        overlayView.layer.opaque = true
+        overlayView.opaque  = true
+        
+        let maskLayer : CAShapeLayer = CAShapeLayer()
+        overlayView.layer.mask = maskLayer
+        
+        
+        presentViewController(imgPicker, animated: true, completion: nil)
+    }
 
+    @IBAction func PhotoLibraryAction(sender: UIButton) {
+        
+        let imgPicker = UIImagePickerController()
+        
+        imgPicker.delegate = self
+        imgPicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imgPicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        ImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
